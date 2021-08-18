b8d36b423e4585bb85ebf03fcae0e6b1b726035e
Jacob Gadikian <jacobgadikian@gmail.com>
2021-08-18 16:57:00 +0700

    fix flags

--- cmd/tenderseed/main.go
+++ cmd/tenderseed/main.go
@@ -22,12 +22,14 @@ func main() {
 
 	homeDir := flag.String("home", filepath.Join(userHomeDir, ".tenderseed"), "path to tenderseed home directory")
 	configFile := flag.String("config", "config/config.toml", "path to configuration file within home directory")
-	chainID := flag.String("chain-id", "osmosis-1", "Chain ID.  Defaults to osmosis-1.")
-	seeds := flag.String("seed", "2e3e3b7703a598024a2fb287587095bc4d14fe52@95.217.196.54:2000,f5be19f84deb843c18e9b612b7987138ba13ac02@5.9.106.185:2000,f9c49739f0641a0a673e7a1e8edc38054fefc840@144.76.183.180:2000,40aafcd9b6959d58dd1c567d9daf2a82a23311cf@162.55.132.230:2000", "")
+	chainID := flag.String("chain-id", "osmosis-1", "Chain ID")
+	seeds := flag.String("seeds", "2e3e3b7703a598024a2fb287587095bc4d14fe52@95.217.196.54:2000,f5be19f84deb843c18e9b612b7987138ba13ac02@5.9.106.185:2000,f9c49739f0641a0a673e7a1e8edc38054fefc840@144.76.183.180:2000,40aafcd9b6959d58dd1c567d9daf2a82a23311cf@162.55.132.230:2000", "Comma separated list of seeds.")
 
-	seedslice := strings.Split(*seeds, ",")
 	// parse top level flags
 	flag.Parse()
+
+	//split the seeds after parsing the flags.
+	seedslice := strings.Split(*seeds, ",")
 
 	configFilePath := filepath.Join(*homeDir, *configFile)
 	tenderseed.MkdirAllPanic(filepath.Dir(configFilePath), os.ModePerm)
@@ -40,7 +42,7 @@ func main() {
 	subcommands.ImportantFlag("home")
 	subcommands.ImportantFlag("config")
 	subcommands.ImportantFlag("chain-id")
-	subcommands.ImportantFlag("seed")
+	subcommands.ImportantFlag("seeds")
 
 	subcommands.Register(subcommands.HelpCommand(), "")
 	subcommands.Register(&cmd.StartArgs{
