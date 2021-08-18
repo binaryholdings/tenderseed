0287dd9bec34dc0f7cbcd132c081fe9c70f78be2
Jacob Gadikian <jacobgadikian@gmail.com>
2021-08-18 13:57:34 +0700

    takes a slice of seed addresses as a flag.  Chain ID can be used as a flag too.  Seeks 60 outgoing by default.

--- cmd/tenderseed/main.go
+++ cmd/tenderseed/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/binaryholdings/tenderseed/internal/cmd"
 	"github.com/binaryholdings/tenderseed/internal/tenderseed"
@@ -21,7 +22,10 @@ func main() {
 
 	homeDir := flag.String("home", filepath.Join(userHomeDir, ".tenderseed"), "path to tenderseed home directory")
 	configFile := flag.String("config", "config/config.toml", "path to configuration file within home directory")
+	chainID := flag.String("chain-id", "osmosis-1", "Chain ID.  Defaults to osmosis-1.")
+	seeds := flag.String("seed", "2e3e3b7703a598024a2fb287587095bc4d14fe52@95.217.196.54:2000", "")
 
+	seedslice := strings.Split(*seeds, ",")
 	// parse top level flags
 	flag.Parse()
 
@@ -35,11 +39,15 @@ func main() {
 
 	subcommands.ImportantFlag("home")
 	subcommands.ImportantFlag("config")
+	subcommands.ImportantFlag("chain-id")
+	subcommands.ImportantFlag("seed")
 
 	subcommands.Register(subcommands.HelpCommand(), "")
 	subcommands.Register(&cmd.StartArgs{
 		HomeDir:    *homeDir,
 		SeedConfig: *seedConfig,
+		ChainID:    *chainID,
+		Seeds:      seedslice,
 	}, "")
 	subcommands.Register(&cmd.ShowNodeIDArgs{
 		HomeDir:    *homeDir,
